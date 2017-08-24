c0c00e5a43648392a591c441d08f239398e2f017
David Simowitz <david.simowitz@gmail.com>
2017-08-23 22:16:37 -0400

    refactor: reorder imports

--- models.py
+++ models.py
@@ -1,9 +1,9 @@
 import sys
 
+from sqlalchemy import create_engine
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
-from sqlalchemy import create_engine
 
 
 Base = declarative_base()
