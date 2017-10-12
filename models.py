656cfeb01292c1dcc14a136f3f3f13dd1e7c81d1
David Simowitz <david.simowitz@gmail.com>
2017-10-11 20:30:30 -0400

    feat: add date and time columns to table

--- models.py
+++ models.py
@@ -2,6 +2,7 @@ import sys
 
 from sqlalchemy import create_engine
 from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Date, Time
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
@@ -25,6 +26,10 @@ class Event(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
     description = Column(String(250))
+    start_date = Column(Date)
+    start_time = Column(Time)
+    end_date = Column(Date)
+    end_time = Column(Time)
     activity_id = Column(Integer, ForeignKey('activity.id'))
     activity = relationship(Activity)
 
--- populate_events_db.py
+++ populate_events_db.py
@@ -24,15 +24,23 @@ def create_events(session):
     """create and commit entries to the event table"""
 
     event_lookup = {}
-    event_lookup['outdoors'] = ['camping', 'hiking', 'beach', 'park']
-    event_lookup['fitness'] = ['saturday morning group run', 'biking',
-                               'swimming', 'walking in the park']
-    event_lookup['games'] = ['game day', 'board game night', 'US Open']
-    event_lookup['music'] = ['concert']
-    event_lookup['film'] = ['movies']
-    event_lookup['food'] = ['birthday dinner', 'Brooklyn Bridge Park BBQ']
-    event_lookup['drink'] = ['happy hour']
-    event_lookup['shopping'] = ['back to school sale']
+    event_lookup['outdoors'] = [('camping', '2017-10-6', '18:30', '2017-10-8', '10:45'),
+                                ('hiking', '2017-10-7', '11:45', '2017-10-7', '17:00'),
+                                ('beach', '2017-10-14', '8:30', '2017-10-14', '15:15'),
+                                ('park', '2017-10-21', '2:15', '2017-10-21', '6:00')]
+    event_lookup['fitness'] = [('saturday morning group run', '2017-9-30', '7:00', '2017-9-30', '9:00'),
+                               ('biking', '2017-10-17', '18:30', '2017-10-17', '19:30'),
+                               ('swimming', '2017-8-19', '10:15', '2017-8-19', '12:15'),
+                               ('walking in the park', '2017-10-19', '17:45', '2017-10-19', '18:15')]
+    event_lookup['games'] = [('game day', '2017-10-22', '13:15', '2017-10-22', '19:45'),
+                             ('board game night', '2017-10-14', '17:00', '2017-10-14', '23:00'),
+                             ('US Open', '2017-8-28', '00:00', '2017-9-1', '23:59')]
+    event_lookup['music'] = [('concert', '2017-11-3', '20:00', '2017-11-3', '24:00')]
+    event_lookup['film'] = [('movies', '2017-12-15', '18:00', '2017-12-15', '20:45')]
+    event_lookup['food'] = [('birthday dinner', '2017-11-25', '18:45', '2017-11-25', '21:45'),
+                            ('Brooklyn Bridge Park BBQ', '2017-10-27', '18:15', '2017-10-27', '19:30')]
+    event_lookup['drink'] = [('happy hour', '2017-10-26', '17:00', '2017-10-26', '18:00')]
+    event_lookup['shopping'] = [('back to school sale', '2017-8-14', '00:00', '2017-8-28', '23:59')]
 
     activities = session.query(Activity)
 
@@ -40,9 +48,13 @@ def create_events(session):
         events = event_lookup[activity_entry.name]
 
         for event in events:
-            new_event = Event(name='{}'.format(event),
+            new_event = Event(name='{}'.format(event[0]),
                               activity=activity_entry,
-                              description='{} event description.'.format(event))
+                              description='{} event description.'.format(event[0]),
+                              start_date='{}'.format(event[1]),
+                              start_time='{}'.format(event[2]),
+                              end_date='{}'.format(event[3]),
+                              end_time='{}'.format(event[4]))
             session.add(new_event)
             session.commit()
 
@@ -70,7 +82,14 @@ def print_events(session):
     print('\nEvents: ')
     for event in events:
         activity = session.query(Activity).filter_by(id=event.activity_id).one()
-        print('{} -> {}: {}'.format(activity.name.title(), event.name, event.description))
+        print('{} -> {}:\n          {}\n          start: {} {}\n          end:   {} {}'.format(
+              activity.name.title(),
+              event.name,
+              event.description,
+              event.start_date,
+              event.start_time,
+              event.end_date,
+              event.end_time))
 
 
 def print_db(session):
